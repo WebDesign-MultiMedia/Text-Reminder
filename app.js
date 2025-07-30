1d93396b2697196d5bc4b5a8ae874f70c893845a
Julio <youremail@example.com>
2025-07-30 14:09:58 -0400

    input func modify

--- app.js
+++ app.js
@@ -14,16 +14,24 @@ const reminderForm = {
 const inputs = document.querySelectorAll('input');
 const reasonTextarea = document.querySelectorAll('textarea');
 const placeholderinput = document.getElementById('reason')
+const btn =  document.getElementById('btn');
+// const head = document.getElementById('header');
 
 if(!reminderForm.date || reminderForm.time || !reminderForm.textReminder || !reminderForm.reasoning){
     alert('ERRRRR')
     inputs.forEach(input => {
         input.style.backgroundColor = 'lightcoral';
         input.style.color = 'white';
+        input.style.border = '1px solid white';
     })
     reasonTextarea.forEach(reasonTextareas =>{
         reasonTextareas.style.backgroundColor = 'lightcoral';
+        reasonTextareas.style.border = '1px solid white';
     })
+    btn.style.backgroundColor = 'lightcoral';
+    btn.style.border = '1px solid white';
+    btn.style.color = 'white';
+
     placeholderinput.classList.add('blue-placeholder');
     return;
 } else{
